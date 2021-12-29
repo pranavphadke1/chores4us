ef04838085e5c9512db02333277fc278d5a09c88
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2021-12-29 18:19:52 -0500

    Formatting changes

--- src/App.js
+++ src/App.js
@@ -14,82 +14,81 @@ function App() {
   const [descriptionText, setDescriptionText] = useState("")
   const [assigneeText, setAssigneeText] = useState("")
   const [dueDateText, setDueDateText] = useState("")
-  const handleChange = event => 
-  {
-    setTitleText( event.value );
-    setDescriptionText( event.value );
-    setAssigneeText( event.value );
-    setDueDateText( event.value );
+  const handleChange = event => {
+    setTitleText(event.value);
+    setDescriptionText(event.value);
+    setAssigneeText(event.value);
+    setDueDateText(event.value);
   };
 
   return (
     <React.Fragment>
-    <div className="App">
-      <div className="tasks-container" >
-        <div className="tasks-title" > Chores4Us </div>
-        <img className="add-task-button" src={buttonpng} />
-        <img className="add-check-button" src={checkpng} />
-      </div>
+      <div className="App">
+        <div className="tasks-container" >
+          <div className="tasks-title" > Chores4Us </div>
+          <img className="add-task-button" src={buttonpng} />
+          <img className="add-check-button" src={checkpng} />
+        </div>
 
-      <div className="add-task-container" >
-        <div className="add-task-title" > Add Task</div>
-        <div className="title" > Title: </div>
-        <div className="title-box">
-          <form method="post" action-xhr="/submit-form" target="_top" class="main-form">
-            <div class="input">
+        <div className="add-task-container" >
+          <div className="add-task-title" > Add Task</div>
+          <div className="title" > Title: </div>
+          <div className="title-box">
+            <form method="post" action-xhr="/submit-form" target="_top" class="main-form">
+              <div class="input">
                 <input type="text" name="name" id="title-box-form" value={titleText} onChange={handleChange} required />
                 <label for="form-name"></label>
-            </div>
-          </form>
-        </div>
-        <div className="description"> Description:</div>
-        <div className="description-box">
-        <form method="post" action-xhr="/submit-form" target="_top" class="main-form">
-            <div class="input">
-                <input type="text" name="name" id="description-box-form" value={descriptionText} onChange={handleChange} required/>
+              </div>
+            </form>
+          </div>
+          <div className="description"> Description:</div>
+          <div className="description-box">
+            <form method="post" action-xhr="/submit-form" target="_top" class="main-form">
+              <div class="input">
+                <input type="text" name="name" id="description-box-form" value={descriptionText} onChange={handleChange} required />
                 <label for="form-name"></label>
-            </div>
-          </form>
-        </div>
-        <div className="assignee">Assignee:</div>
-        <div className="assignee-box">
-        <form method="post" action-xhr="/submit-form" target="_top" class="main-form">
-            <div class="input">
-                <input type="text" name="name" id="assignee-box-form" value={assigneeText} onChange={handleChange} required/>
+              </div>
+            </form>
+          </div>
+          <div className="assignee">Assignee:</div>
+          <div className="assignee-box">
+            <form method="post" action-xhr="/submit-form" target="_top" class="main-form">
+              <div class="input">
+                <input type="text" name="name" id="assignee-box-form" value={assigneeText} onChange={handleChange} required />
                 <label for="form-name"></label>
-            </div>
-          </form>
-        </div>
-        <div className="due-date">Due Date:</div>
-        <div className="due-date-box">
-        <form method="post" action-xhr="/submit-form" target="_top" class="main-form">
-            <div class="input">
-                <input type="text" name="name" id="due-date-box-form" value={dueDateText} onChange={handleChange} required/>
+              </div>
+            </form>
+          </div>
+          <div className="due-date">Due Date:</div>
+          <div className="due-date-box">
+            <form method="post" action-xhr="/submit-form" target="_top" class="main-form">
+              <div class="input">
+                <input type="text" name="name" id="due-date-box-form" value={dueDateText} onChange={handleChange} required />
                 <label for="form-name"></label>
-            </div>
-          </form>
+              </div>
+            </form>
+          </div>
+          <div className="frequency">Frequency:</div>
+          <div className="daily-button"></div>
+          <div className="daily">Daily</div>
+          <div className="weekly-button"></div>
+          <div className="weekly">Weekly</div>
+          <div className="bi-weekly-button"></div>
+          <div className="bi-weekly">Bi-Weekly</div>
         </div>
-        <div className="frequency">Frequency:</div>
-        <div className="daily-button"></div>
-        <div className="daily">Daily</div>
-        <div className="weekly-button"></div>
-        <div className="weekly">Weekly</div>
-        <div className="bi-weekly-button"></div>
-        <div className="bi-weekly">Bi-Weekly</div>
-      </div>
 
-      <div className="completed-container" >
-        <div className="completed-tasks-title" > Completed Tasks </div>
-        <img className="add-back-arrow-button" src={backarrow} />
-        
-      </div>
+        <div className="completed-container" >
+          <div className="completed-tasks-title" > Completed Tasks </div>
+          <img className="add-back-arrow-button" src={backarrow} />
 
-      <div className="add-task-container">
-        <img className="cancel-button" src={cancelpng} />
-        <img className="mark-completed-task-button" src={addTaskpng} />
-      </div>
+        </div>
 
-    </div>
+        <div className="add-task-container">
+          <img className="cancel-button" src={cancelpng} />
+          <img className="mark-completed-task-button" src={addTaskpng} />
+        </div>
+
+      </div>
     </React.Fragment>
 
   );
